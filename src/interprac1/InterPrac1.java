13d22b9d002bda2e624c3b016b7f227bbc809762
Ulises <Ulises@DESKTOP-UGTTJG4>
2025-07-17 20:43:18 -0600

    Primera practica de intersemestral

---
+++ src/interprac1/InterPrac1.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package interprac1;
+
+public class InterPrac1 { 
+    public static void main(String[] args) {
+        
+        PrincipalJFrame principal = new PrincipalJFrame();
+        principal.setVisible(true);
+    }
+    
+}
